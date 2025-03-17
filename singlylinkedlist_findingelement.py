45d2c9d7645058077332a9fd94cdd9ebaa409b70
Naveenkumar200326 <naveenkumar200326@gmail.com>
2025-03-17 10:34:37 +0530

    Create singlylinkedlist_findingelement.py

---
+++ singlylinkedlist_findingelement.py
@@ -0,0 +1,45 @@
+
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.next=None
+class singly:
+  def __init__(self):
+    self.head=None
+  def insertatend(self,data):
+    newnode=Node(data)
+    if(self.head==None):
+      self.head=newnode
+      return
+    temp=self.head
+    while temp.next!=None:
+      temp=temp.next
+    temp.next=newnode
+  
+  def findelement(self,pos):
+    count=0
+    temp=self.head
+    while temp:
+      count+=1
+      temp=temp.next
+    
+    if pos>count or pos<=0:
+      print("Invalid position")
+      return
+    
+    index=count-pos
+    temp=self.head
+    for i in range(index):
+      temp=temp.next
+    print(temp.data)
+    
+    
+    
+s=singly()
+x=0
+while x!=-1:
+  x=int(input())
+  if x!=-1:
+    s.insertatend(x)
+pos=int(input())
+s.findelement(pos)
